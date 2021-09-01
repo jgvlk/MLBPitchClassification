abd427d4b49ab48c475e8a184e276108f32a4128
Jonathan Vlk <jonathan.vlk10@gmail.com>
2021-09-01 15:49:20 -0500

    added script to export raw data

---
+++ src/PyPitch/DB/data.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+
+from src.PyPitch.db import query_raw_data
+
+
+ret, df = query_raw_data()
+
+cwd = Path.cwd()
+
+out_file = cwd / 'MLBPitchData_2019.csv'
+
+df.to_csv(out_file)
+
